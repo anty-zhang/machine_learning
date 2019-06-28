19532bd130a475ff0ef7efa60fce88202ee26c46
zhangguoqiang <zhangguoqiang@EBJ1202163.local>
2019-06-28 19:16:01 +0800

    ensemble learning

---
+++ notebook_start.sh
@@ -0,0 +1,11 @@
+/*************************************************************************
+  > File Name: notebook_start.sh
+  > Author: 
+  > Mail: 
+  > Created Time: 2019-06-28 15:55:01
+ ************************************************************************/
+
+# jupyter notebook password
+nohup jupyter notebook --port=9100 &
+
+
--- resource/compete.md
+++ resource/compete.md
@@ -6,3 +6,4 @@ http://www.dcjingsai.com
 http://suanfazu.com/t/kaggle/230 (Kaggle 机器学习竞赛冠军及优胜者的源代码汇总) 
 https://blog.csdn.net/qq_34264472/article/details/53363384 (xgboost特征选择)
 
+Netflix、KDD 2009 和 Kaggle 比赛
